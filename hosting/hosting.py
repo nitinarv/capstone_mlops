0e7ce0ace78e342ada8fc06e496ddfcfdb5cd5b7
DESKTOP-JSBMGVF\Nitin <nitin.v@pixelintelligence.net>
2026-02-07 21:55:10 +0530

    fixing hosting path

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -6,7 +6,7 @@ repo_type = "space"
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="mlops/deployment",     # the local folder containing your files
+    folder_path="deployment",     # the local folder containing your files
     repo_id=repo_id,          # the target repo
     repo_type=repo_type,                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
